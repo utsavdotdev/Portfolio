bd9734967a7c35e7646f9ed63c6988154f0d25b9
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-03-09 20:01:49 +0545

    h3llo

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import Loader from '../components/loader';
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Utsav Bhattarai</title>
         <meta name="description" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to my Website
-        </h1>
-
-        <p className={styles.description}>
-          Home-page
-        </p>
-      </main>
-       
-
-    
-    </div>
+      <Loader/>
+    </>
   )
 }
